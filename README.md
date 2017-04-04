a7fd324065e1b6fb951d7012fa13e98d3e3aeab5
Ralf S. Engelschall <rse@engelschall.com>
2017-04-04 23:53:03 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-config-webpack
-Webpack Bundling Configuration
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Config Webpack
+=======================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-config-webpack.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-config-webpack.png" alt=""/>
+
+About
+-----
+
+This is the [Webpack](http://webpack.js.org/) frontend bundling configuration for the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It consists of an elabortated Webpack configuration, using a associated Gemstone specific Webpack loaders.
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-config-webpack.js
@@ -0,0 +1,451 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+const path              = require("path")
+const fs                = require("fs-promise")
+const rimraf            = require("rimraf")
+const gemstoneConfig    = require("gemstone-config")
+const webpack           = require("webpack")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const PreBuildPlugin    = require("pre-build-webpack")
+const OnBuildPlugin     = require("on-build-webpack")
+const FaviconsPlugin    = require("favicons-webpack-plugin")
+const Chalk             = require("chalk")
+const Progress          = require("progress")
+const stripIndent       = require("strip-indent")
+const jsBeautify        = require("js-beautify")
+const hashFiles         = require("hash-files")
+
+module.exports = function (opts) {
+    /*  determine Gemstone configuration  */
+    let cfg = gemstoneConfig()
+
+    /*  get a chalk instance  */
+    const chalk = new Chalk.constructor({ enabled: process.stderr.isTTY })
+
+    /*  instanciate progress bar  */
+    let progressCur = 0.0
+    let progressBar = new Progress(`   compiling: [:bar] ${chalk.bold(":percent")} (elapsed: :elapseds) :msg `, {
+        complete:   chalk.bold.green("#"),
+        incomplete: "=",
+        width:      20,
+        total:      1.0,
+        stream:     process.stderr
+    })
+
+    /*  determine copyright message  */
+    let copyright = cfg.header
+    copyright = copyright
+        .replace(/[ \t]+$/mg, "")                        /*  remove trailing blanks to simplify multi-line matching  */
+        .replace(/^(?:\r?\n)+/, "").replace(/^/, "\n")   /*  ensure a single preceeding blank line  */
+        .replace(/(?:\r?\n)+$/, "").replace(/$/, "\n\n") /*  ensure a single following blank line  */
+        .replace(/^/mg, "    ").replace(/\n *$/, "\n")   /*  prefix all lines the comment block  */
+        .replace(/[ \t]+$/mg, "")                        /*  remove (potentially generated) trailing blanks  */
+
+    /*  generate HTML index page skeleton  */
+    let index = stripIndent(`
+        <!DOCTYPE html>
+        <!--
+        %copyright%
+        -->
+        <html>
+            <head>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, maximum-scale=1, user-scalable=no">
+                <meta name="mobile-web-app-capable" content="yes">
+                <meta name="apple-mobile-web-app-capable" content="yes">
+                <meta name="apple-touch-fullscreen" content="yes">
+                <meta name="robots" content="noindex, nofollow, noarchive">
+                <meta name="generator" content="Gemstone">
+                <title>${cfg.meta.title}</title>
+                <meta name="description" content="${cfg.meta.description}">
+                <meta name="author" content="${cfg.meta.author}">
+                <meta name="keywords" content="${cfg.meta.keywords}">
+            </head>
+            <body>
+            </body>
+        </html>
+    `  ).replace(/%copyright%\n/, copyright)
+        .replace(/^\n+/, "")
+        .replace(/([ \t]*\n)+[ \t]*$/, "\n")
+
+    /*  FIXME: <script type="text/javascript">App.default.main()</script>  */
+
+    /*  determine resolved path to source files  */
+    let sourceResolved = path.resolve(cfg.path.source)
+
+    /*  the SVG image/font checking cache  */
+    const svgIsFont = (() => {
+        let isFontCache = {}
+        return (path) => {
+            let isFont = isFontCache[path]
+            if (isFont === undefined) {
+                let svg = fs.readFileSync(path, "utf8")
+                isFont = (svg.match(/<font[^>]*>(.|\r?\n)*<\/font>/) !== null)
+                isFontCache[path] = true
+            }
+            return isFont
+        }
+    })()
+
+    /*  start assembling Webpack configuration object  */
+    let config = {
+        plugins: [
+            new webpack.NoEmitOnErrorsPlugin(),
+            new ExtractTextPlugin({
+                filename:  "[name].css",
+                allChunks: true
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "lib",
+                minChunks: function (module) {
+                    return (
+                        module.context && (module.context.match(/(?:node_modules|bower_components)/)
+                        || module.context.match(/gemstone-framework-frontend/))
+                    )
+                }
+            }),
+            new HTMLWebpackPlugin({
+                templateContent: index,
+                filename:       "index.html",
+                inject:         "head",
+                title:          cfg.meta.title,
+                chunksSortMode: "dependency",
+                cache:          true
+            }),
+            new FaviconsPlugin({
+                logo:             cfg.path.icon,
+                prefix:           "index-",
+                emitStats:        false,
+                persistentCache:  opts.env === "development",
+                inject:           true,
+                background:       "#ffffff",
+                title:            cfg.meta.title,
+                icons: {
+                    android:      true,
+                    appleIcon:    true,
+                    appleStartup: false,
+                    favicons:     true,
+                    firefox:      false,
+                    coast:        false,
+                    opengraph:    false,
+                    twitter:      false,
+                    yandex:       false,
+                    windows:      false
+                }
+            }),
+            new webpack.BannerPlugin({
+                banner:    cfg.header,
+                raw:       false,
+                entryOnly: true
+            }),
+            new webpack.ProgressPlugin((percentage, msg) => {
+                if (msg.length > 40)
+                    msg = msg.substr(0, 40) + "..."
+                let delta = percentage - progressCur
+                progressBar.tick(delta, { msg })
+                if (progressBar.complete)
+                    process.stderr.write("\n")
+                progressCur += delta
+            }),
+            new PreBuildPlugin(async (/* stats */) => {
+                if (opts.env === "production") {
+                    /*  remove destination directory (recursively)  */
+                    if (await fs.exists(cfg.path.output)) {
+                        await new Promise((resolve, reject) => {
+                            rimraf(cfg.path.output, {}, (err) => {
+                                if (err) reject(err)
+                                else     resolve()
+                            })
+                        })
+                    }
+                }
+            }),
+            new OnBuildPlugin(async (/* stats */) => {
+                /*  remove unwanted generated file  */
+                let manifest = path.join(cfg.path.output, "index-manifest.json")
+                if (await fs.exists(manifest))
+                    await fs.unlink(manifest)
+
+                /*  reformat index.html file  */
+                let filename = path.join(cfg.path.output, "index.html")
+                if (await fs.exists(filename)) {
+                    let html = await fs.readFile(filename, "utf8")
+                    html = jsBeautify.html(html, {
+                        indent_size: 4,
+                        indent_char: " ",
+                        indent_inner_html: true,
+                        extra_liners: []
+                    })
+                    await fs.writeFile(filename, html, "utf8")
+                }
+            })
+        ],
+        context: process.cwd(),
+        entry: {
+            "app": cfg.path.main
+        },
+        resolve: {
+            modules: cfg.modules.source.concat([
+                cfg.path.source,
+                "node_modules",
+                "bower_components"
+            ]),
+            descriptionFiles: [
+                "package.json",
+                "bower.json"
+            ],
+            mainFields: [
+                "browser",
+                "main"
+            ],
+            alias: {
+                "gemstone$": "gemstone-framework-frontend"
+            }
+        },
+        externals: [{
+            "navigator":    "navigator",
+            "window":       "window",
+            "document":     "document",
+            "websocket":    "WebSocket"
+        }],
+        module: {
+            noParse: /\/gemstone-framework-frontend\//,
+            rules: [
+                /*  ==== LIB ====  */
+                {
+                    test: (path) => {
+                        return path.match(/\/(?:node_modules|bower_components)\//)
+                    },
+                    rules: [
+                        {   test: /\.js/,
+                            rules: [
+                                {
+                                    parser: {
+                                        amd: false,
+                                        commonjs: true
+                                    }
+                                }
+                            ]
+                        },
+                        /*  CSS/LESS  */
+                        {   test: /\.css$/,
+                            use: ExtractTextPlugin.extract({
+                                fallback: require.resolve("style-loader"),
+                                use: require.resolve("css-loader")
+                            })
+                        },
+                        /*  JPEG/PNG/GIF images  */
+                        {   test: /\.(?:jpg|png|gif)$/,
+                            use: {
+                                loader: require.resolve("file-loader"),
+                                options: "name=lib-img-[md5:hash:base62:32].[ext]"
+                            }
+                        },
+                        /*  SVG images/fonts  */
+                        {   test: /\.svg$/,
+                            rules: [ {
+                                test: (path) => !svgIsFont(path),
+                                use: {
+                                    loader: require.resolve("file-loader"),
+                                    options: "name=lib-img-[md5:hash:base62:32].[ext]"
+                                }
+                            }, {
+                                test: (path) => svgIsFont(path),
+                                use: {
+                                    loader: require.resolve("file-loader"),
+                                    options: "name=lib-font-[md5:hash:base62:32].[ext]"
+                                }
+                            } ]
+                        },
+                        /*  EOT/WOFF/TTF fonts  */
+                        {   test: /\.(?:eot|woff2?|ttf)$/,
+                            use: {
+                                loader: require.resolve("file-loader"),
+                                options: "name=lib-font-[md5:hash:base62:32].[ext]"
+                            }
+                        }
+                    ]
+                },
+
+                /*  ==== APP ====  */
+                {
+                    test: (path) => {
+                        return (path.indexOf(sourceResolved) === 0)
+                    },
+                    rules: [
+                        /*  JavaScript  */
+                        {   test:    /\.js$/,
+                            exclude: (path) => {
+                                return (
+                                       path.match(/(?:node_modules|bower_components)/)
+                                    /* && !path.match(/componentjs-mvc/) */
+                                )
+                            },
+                            use: require.resolve("gemstone-loader-js")
+                        },
+                        /*  HTML  */
+                        {   test:   /\.html$/,
+                            use: require.resolve("gemstone-loader-html")
+                        },
+                        /*  YAML  */
+                        {   test:   /\.yaml$/,
+                            use: require.resolve("gemstone-loader-yaml")
+                        },
+                        /*  CSS/LESS  */
+                        {   test:   /\.(?:less|css)$/,
+                            use: ExtractTextPlugin.extract({
+                                fallback: require.resolve("style-loader"),
+                                use: require.resolve("gemstone-loader-css")
+                            })
+                        },
+                        /*  JPEG/PNG/SVG images  */
+                        {   test:   /\.(?:jpg|png|svg)$/,
+                            use: require.resolve("url-loader")
+                        }
+                    ]
+                }
+            ]
+        },
+        target: "web",
+        output: {
+            path:              path.resolve(cfg.path.output),
+            filename:          "[name].js",
+            // sourceMapFilename: "[name].map",
+            libraryTarget:     "var",
+            library:           "App",
+            publicPath:        ""
+        },
+        stats: {
+            colors:          chalk.supportsColor,
+            hash:            false,
+            version:         false,
+            timings:         false,
+            warnings:        false,
+            errors:          true,
+            errorDetails:    true,
+
+            assets:          false,
+            assetsSort:      "chunks",
+            children:        false,
+            cached:          false,
+            cachedAssets:    false,
+
+            entrypoints:     true,
+
+            chunks:          true,
+            chunkModules:    true,
+            chunkOrigins:    false,
+            chunksSort:      "",
+
+            modules:         true,
+            modulesSort:     "",
+            maxModules:      Infinity,
+            exclude:         [ "node_modules", "bower_components" ],
+            usedExports:     false,
+            providedExports: false,
+            performance:     true,
+            publicPath:      false,
+            reasons:         false,
+            source:          false
+        }
+    }
+
+    /*  provide Webpack module aliasing  */
+    cfg.modules.alias.forEach((alias) => {
+        config.resolve.alias[alias.from] = alias.to
+    })
+
+    /*  provide Webpack module loaders  */
+    cfg.modules.rules.forEach((rule) => {
+        let uses = []
+        Object.keys(rule.use).forEach((use) => {
+            uses.push({
+                loader: require.resolve(`${use}-loader`),
+                options: rule.use[use]
+            })
+        })
+        config.module.rules[0].rules.unshift({
+            test: new RegExp(rule.test),
+            use: uses
+        })
+    })
+
+    /*  provide Webpack module provides  */
+    let provides = {}
+    cfg.modules.provide.forEach((provide) => {
+        provides[provide.name] = provide.require
+    })
+    config.plugins.push(new webpack.ProvidePlugin(provides))
+
+    /*  provide Webpack module replacements  */
+    cfg.modules.replace.forEach((replace) => {
+        let from = new RegExp(replace.match)
+        let to   = replace.replace
+        if (typeof to === "object" && to instanceof Array) {
+            let substs = to
+            to = function (resource) {
+                substs.forEach((subst) => {
+                    resource.request = resource.request.replace(new RegExp(subst[0]), subst[1])
+                })
+            }
+        }
+        config.plugins.push(new webpack.NormalModuleReplacementPlugin(from, to))
+    })
+
+    /*  determine build hash  */
+    let hash = hashFiles.sync({
+        files: [ `${sourceResolved}/**/*` ],
+        algorithm: "md5"
+    })
+
+    /*  provide environment information  */
+    config.plugins.push(new webpack.DefinePlugin({
+        "process.env": {
+            "NODE_ENV":  `"${opts.env}"`,
+            "NODE_TAG":  `"${opts.tag}"`,
+            "NODE_HASH": `"${hash}"`
+        },
+        "process.config": {
+            "env":      `"${opts.env}"`,
+            "tag":      `"${opts.tag}"`,
+            "hash":     `"${hash}"`
+        }
+    }))
+
+    /*  final environment-specific treatments  */
+    if (opts.env === "production") {
+        /*  compress JS files  */
+        config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            beautify:  false,
+            comments:  false,
+            mangle:    false,
+            compress: {
+                warnings: false
+            }
+        }))
+
+        /*  compress any other files (except for JS files)  */
+        config.plugins.push(new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }))
+    }
+    else {
+        /*  provide source-maps for debugging  */
+        // config.devtool = "source-map"
+        config.plugins.push(new webpack.SourceMapDevToolPlugin({
+            test: /app\.(?:css|js)$/,
+            filename: "[file].map"
+        }))
+    }
+
+    /*  final cleanups  */
+
+    return config
+}
+
