125d4b9199a362ccd5cb96a385b23e01abb071e1
Ralf S. Engelschall <rse@engelschall.com>
2017-04-08 09:52:53 +0200

    fix syntax

--- gemstone-config-webpack.js
+++ gemstone-config-webpack.js
@@ -413,7 +413,7 @@ module.exports = function (opts) {
         files: [ `${sourceResolved}/**/*` ],
         algorithm: "md5"
     })
-    hash = hash.replace(/([0-9a-f]{8})(?=.)/g, "$1-"))
+    hash = hash.replace(/([0-9a-f]{8})(?=.)/g, "$1-")
 
     /*  determine build time  */
     let time = Moment(new Date()).format("YYYY-MM-DD-hh-mm-ss")
